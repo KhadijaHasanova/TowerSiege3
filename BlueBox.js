08191d10b1b7bd67e9f06d186ddeb4f2328ec559
KhadijaHasanova <74422542+KhadijaHasanova@users.noreply.github.com>
2021-01-18 17:17:17 -0600

    Update BlueBox.js

--- BlueBox.js
+++ BlueBox.js
@@ -1,39 +1,26 @@
 class BlueBox {
   constructor(x, y, width, height) {
     var options = {
-        'restitution':0.8,
-        'friction':1,
-        'density':1
+        'restitution':0.4,
+        'friction':0,
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
-
-    var visibility = 225;
     
     World.add(world, this.body);
   }
-
   display(){
-    console.log(this.body.speed);
-    if(this.body.speed < 3) {
-      var pos = this.body.position;
-      var angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      stroke("black");
-      strokeWeight(3);
-      fill(0,0,255)
-      rect(0, 0, this.width, this.height);
-      pop();
-    }
-    else {
-      World.remove(world,this.body);
-      push();
-      this.visibility = this.visibility - 5;
-      pop();
-    }
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    stroke("black");
+    strokeWeight(3);
+    fill(0,0,255)
+    rect(0, 0, this.width, this.height);
+    pop();
   }
 };
