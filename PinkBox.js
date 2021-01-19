aff0efbb0b81dadf61c50fb2440c1a175bc58695
KhadijaHasanova <74422542+KhadijaHasanova@users.noreply.github.com>
2021-01-19 08:49:12 -0600

    Update PinkBox.js

--- PinkBox.js
+++ PinkBox.js
@@ -1,16 +1,21 @@
 class PinkBox {
-    constructor(x, y, width, height) {
-      var options = {
+  constructor(x, y, width, height) {
+    var options = {
         'restitution':0.4,
         'friction':0,
     }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      
-      World.add(world, this.body);
-    }
-    display(){
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+
+    var visibility = 225;
+    
+    World.add(world, this.body);
+  }
+
+  display(){
+    console.log(this.body.speed);
+    if(this.body.speed < 3) {
       var pos = this.body.position;
       var angle = this.body.angle;
       push();
@@ -19,8 +24,15 @@ class PinkBox {
       rectMode(CENTER);
       stroke("black");
       strokeWeight(3);
-      fill(255,0,255);
+      fill(255,0,255)
       rect(0, 0, this.width, this.height);
       pop();
     }
-  };
+    else {
+      World.remove(world,this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      pop();
+    }
+  }
+};
